983173d87eacea907f33a77a6b5e91b10d65a713
David Abellán <davidabellan89@gmail.com>
2022-04-13 19:22:35 +0200

    Exercicis per a controlar els bucles

---
+++ ExercicisBucles.py
@@ -0,0 +1,13 @@
+#Imprimir per consola tots els múltiples de 3 menors que 100
+
+
+#Imprimir per consola múltiples de 7 fins a sumar 100 (entre el que has imprimit)
+
+
+#Imprimir per consola els primers 15 números de la seqüència de Fibonazi
+
+
+#Imprimir per consola els primers 15 números de una seqüència de Fibonazi a la que podem passar per paràmetre dos
+#valors inicials
+
+
